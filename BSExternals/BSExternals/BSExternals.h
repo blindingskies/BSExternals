b7b52e316e60dd663d70b5a7651a9d0b4c4a9ad4
Daniel Thorpe <dan@blindingskies.com>
2013-05-05 22:58:50 +0100

    Updates the BSExternals with CocoaLumberJack
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

---
+++ BSExternals/BSExternals/BSExternals.h
@@ -0,0 +1,18 @@
+//
+//  BSExternals.h
+//  BSExternals
+//
+//  Created by Daniel Thorpe on 05/05/2013.
+//  Copyright (c) 2013 Blinding Skies. All rights reserved.
+//
+
+#ifndef BSExternals_BSExternals_h
+#define BSExternals_BSExternals_h
+
+#import "DDLog.h"
+#import "DDAbstractDatabaseLogger.h"
+#import "DDASLLogger.h"
+#import "DDFileLogger.h"
+#import "DDTTYLogger.h"
+
+#endif
