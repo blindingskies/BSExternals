39e81c6800daf4da8ad93b91e7a2e6024519085d
Daniel Thorpe <dan@blindingskies.com>
2013-05-05 20:14:14 +0100

    Sets up BSExternals project.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

---
+++ BSExternals/BSExternalsTests/BSExternalsTests.h
@@ -0,0 +1,13 @@
+//
+//  BSExternalsTests.h
+//  BSExternalsTests
+//
+//  Created by Daniel Thorpe on 05/05/2013.
+//  Copyright (c) 2013 Blinding Skies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BSExternalsTests : SenTestCase
+
+@end
---
+++ BSExternals/BSExternalsTests/BSExternalsTests.m
@@ -0,0 +1,32 @@
+//
+//  BSExternalsTests.m
+//  BSExternalsTests
+//
+//  Created by Daniel Thorpe on 05/05/2013.
+//  Copyright (c) 2013 Blinding Skies. All rights reserved.
+//
+
+#import "BSExternalsTests.h"
+
+@implementation BSExternalsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BSExternalsTests");
+}
+
+@end
